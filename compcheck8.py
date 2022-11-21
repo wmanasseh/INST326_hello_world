b90403fdb0835643ea5dab961155c500757d7425
wmanasseh <wmanasse@montgomerycollege.edu>
2022-11-20 21:59:32 -0500

    init

---
+++ compcheck8.py
@@ -0,0 +1,15 @@
+# Wel Manasseh 
+# Comprehension Check 8
+# INST326_hello_world
+
+#Create a Github account if you do not already have one. They are free. https://github.com/ Links to an external site. 
+#Create a new repository and name it whatever you would like. I suggest "INST326_hello_world".
+#Clone your Github repository.
+#Click on 'Code' and copy the https URL
+#Using either the command line or Github Desktop, use the provided URL to clone the repository to your local machine
+#Open the code in VS Code.
+#This is easy to do using Github Desktop, once you have cloned the repository, you can simply click on 'Open in Visual Studio Code' or you can open it just by including the folder in VS Code
+#Add a Python file 
+#Add and Commit the file (do not forget to add a message)
+#Push the file to the remote repository (back to Github). 
+#Provide the link to your Github repository here.
